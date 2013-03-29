70e40a7eceb91cda34991b390a742c94bbb12c13
Ciaron Linstead <ciaron.linstead@gmail.com>
2013-03-29 21:46:32 +0100

    saving the formset

--- djsortable/models.py
+++ djsortable/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
 class Entry(models.Model):
     
+    class Meta:
+        ordering = ['order']
+
     def __unicode__(self):
         return self.name
 
     name = models.CharField(max_length=50)
     data = models.CharField(max_length=200)
     order = models.IntegerField(default=0)
+
+class EntryForm(ModelForm):
+    class Meta:
+        model = Entry
--- djsortable/views.py
+++ djsortable/views.py
@@ -1,17 +1,35 @@
 #from django.template import Context, loader
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse
+from django.forms.models import modelformset_factory
+from django.template import RequestContext
+from django.contrib import messages
 
-from djsortable.models import Entry
+from djsortable.models import Entry, EntryForm
 
 def index(request):
+
+    EntryFormSet = modelformset_factory(Entry, extra=0)
+
+    if request.method == 'POST':
+        formset = EntryFormSet(request.POST)
+
+        if formset.is_valid():
+            formset.save()
+            messages.info(request, "Changes saved!")
+        else:
+            pass
+
     entry_list = Entry.objects.order_by('order')
-    context = {'entry_list': entry_list}
-    return render(request, 'entry/index.html', context)
+    formset = EntryFormSet()
+
+    context = {'entry_list': entry_list, 'formset': formset}
+
+    return render_to_response('entry/index.html', context, context_instance=RequestContext(request))
 
 def detail(request, entry_id):
     try:
         entry = Entry.objects.get(pk=entry_id)
     except Entry.DoesNotExist:
         raise Http404
-    return render(request, 'entry/detail.html', {'entry': entry})
+    return render_to_response('entry/detail.html', {'entry': entry})
