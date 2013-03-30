93f84671b02da77a4e5e596dbef65270cfbeaff6
Ciaron Linstead <ciaron.linstead@gmail.com>
2013-03-30 14:50:35 +0100

    update DB on AJAX POST... woo\!

--- djsortable/views.py
+++ djsortable/views.py
@@ -1,31 +1,28 @@
 #from django.template import Context, loader
 from django.shortcuts import render_to_response
-from django.http import Http404, HttpResponse
-from django.forms.models import modelformset_factory
+from django.http import Http404, HttpResponse, QueryDict
 from django.template import RequestContext
 from django.contrib import messages
 
-from djsortable.models import Entry, EntryForm
+from djsortable.models import Entry
 
 def index(request):
 
-    EntryFormSet = modelformset_factory(Entry, extra=0)
-
     if request.method == 'POST':
-        return HttpResponse("got POST")
+        # request.POST['content'] is a query string like 'entry[]=3&entry[]=2&entry[]=1'
+        # convert to a QueryDict so we can do things with it
+        entries = QueryDict(request.POST['content'])
 
-        #formset = EntryFormSet(request.POST)
+        for index, entry_id in enumerate(entries.getlist('entry[]')):
+            # save index of entry_id as it's new order value
+            entry = Entry.objects.get(id=entry_id)
+            entry.order = index
+            entry.save()
 
-        #if formset.is_valid():
-        #    formset.save()
-        #    messages.info(request, "Changes saved!")
-        #else:
-        #    pass
+        #return HttpResponse("got POST")
 
     entry_list = Entry.objects.order_by('order')
-    formset = EntryFormSet()
 
-    #context = {'entry_list': entry_list, 'formset': formset}
     context = {'entry_list': entry_list}
 
     return render_to_response('entry/index.html', context, context_instance=RequestContext(request))
